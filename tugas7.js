ef91a8d209e715e46e1b4bc4f93cfbdd240ccad4
arif wijanarko <arifpethod@gmail.com>
2023-06-16 08:58:06 +0700

    tugas js 07
    tugas untuk niomic

---
+++ tugas7.js
@@ -0,0 +1,9 @@
+var tinggibadan = [165, 170, 169, 156, 173, 172, 177, 168, 166, 161];
+
+for (a of tinggibadan) {
+    console.log(a)
+}
+
+for (var b = 0; b < tinggibadan.length; b++){
+    console.log(tinggibadan[b])
+}
